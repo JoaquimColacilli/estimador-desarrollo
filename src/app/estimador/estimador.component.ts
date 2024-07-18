80582f1ed416d4114cc4f36b3a0f635890d47e4f
Joaquim <joaquimcolacilli9@gmail.com>
2024-07-18 20:14:04 -0300

    se agregan animaciones y estilos tailwind

--- src/app/estimador/estimador.component.ts
+++ src/app/estimador/estimador.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { EstimadorService } from '../service/estimador.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-estimador',
@@ -11,16 +18,33 @@ import { EstimadorService } from '../service/estimador.service';
   imports: [CommonModule, FormsModule],
 })
 export class EstimadorComponent {
-  desarrolloHoras: number = 0;
-  estimaciones: any;
+  desarrolloHoras: number | null = null;
+  estimaciones: any = null;
+  totalHoras: number = 0;
 
   constructor(private estimadorService: EstimadorService) {}
 
   calcularEstimacion(): void {
-    if (this.desarrolloHoras) {
+    if (this.desarrolloHoras && this.desarrolloHoras > 0) {
       this.estimaciones = this.estimadorService.calcularEstimaciones(
         this.desarrolloHoras
       );
+      this.calcularTotalHoras();
+      console.log(this.desarrolloHoras);
+    } else {
+      this.estimaciones = null;
+      this.totalHoras = 0;
+    }
+  }
+
+  calcularTotalHoras(): void {
+    if (this.estimaciones) {
+      this.totalHoras = (Object.values(this.estimaciones) as number[]).reduce(
+        (acc, val) => acc + val,
+        0
+      );
+    } else {
+      this.totalHoras = 0;
     }
   }
 }
--- src/app/service/estimador.service.ts
+++ src/app/service/estimador.service.ts
@@ -1,4 +1,3 @@
-// src/app/estimador.service.ts
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,18 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class EstimadorService {
   calcularEstimaciones(desarrolloHoras: number): any {
-    const analisisFuncional = desarrolloHoras * 0.1;
-    const analisisTecnico = desarrolloHoras * 0.1;
-    const pruebasUnitarias = desarrolloHoras * 0.1;
-    const pruebasIntegracion = desarrolloHoras * 0.1;
-    const gestion = desarrolloHoras * 0.15;
+    const analisisFuncional = Math.round(desarrolloHoras * 0.1);
+    const analisisTecnico = Math.round(desarrolloHoras * 0.1);
+    const pruebasUnitarias = Math.round(desarrolloHoras * 0.1);
+    const pruebasIntegracion = Math.round(desarrolloHoras * 0.1);
+    const implementacionYSoporte = Math.round(desarrolloHoras * 0.15);
+    const gestion = Math.round(desarrolloHoras * 0.15);
 
     return {
       analisisFuncional,
       analisisTecnico,
-      desarrollo: desarrolloHoras,
+      desarrollo: Math.round(desarrolloHoras),
       pruebasUnitarias,
       pruebasIntegracion,
+      implementacionYSoporte,
       gestion,
     };
   }
