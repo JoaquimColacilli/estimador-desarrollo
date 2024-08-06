35932b0574c4c61e215782a9a18d0514d88e5bad
Joaquim <joaquimcolacilli9@gmail.com>
2024-08-06 11:28:33 -0300

    se guarda en docs

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-UQMRJQYV.mjs";import"./chunk-WYQAAAVC.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
+import{a}from"./chunk-4JR77REU.mjs";import"./chunk-WYQAAAVC.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
