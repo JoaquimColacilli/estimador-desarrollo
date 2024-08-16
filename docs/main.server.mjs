2e177729d9d74b238582b5b9f6b2b0d41127ad9a
Joaquim <joaquimcolacilli9@gmail.com>
2024-08-15 23:30:41 -0300

    se hace build

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KDQMLEQU.mjs";import"./chunk-HLESK2XM.mjs";import"./chunk-CFU7QGLS.mjs";import"./chunk-VEECDVLU.mjs";import"./chunk-66D3LD7O.mjs";export{a as default};
+import{a}from"./chunk-BPKNTAPT.mjs";import"./chunk-YLNTFLNA.mjs";import"./chunk-CFU7QGLS.mjs";import"./chunk-VEECDVLU.mjs";import"./chunk-66D3LD7O.mjs";export{a as default};
